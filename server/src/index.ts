39d9d808d0b7c8a316f16282000112f436e77981
Kristian2109 <kristian.petrov1998@gmail.com>
2024-03-01 17:39:32 +0200

    Confiugre dotenv with a port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors"
+import dotenv from 'dotenv'
+
+dotenv.config()
+const PORT = process.env.PORT || 8080;
 
 const app: Application = express();
 
@@ -8,6 +12,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000, () => {
-    console.log("server is listening on port: " + 3000);
+app.listen(PORT, () => {
+    console.log("server is listening on port: " + PORT);
 });
